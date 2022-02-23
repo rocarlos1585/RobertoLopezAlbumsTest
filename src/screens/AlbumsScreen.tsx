c625e20635988f892d1a95e496a4239d7e5d10d1
Roberto Carlos Lopez Perez <roberto.karlos.lopez@gmail.com>
2022-02-22 23:41:27 -0600

    project created, first commit

---
+++ src/screens/AlbumsScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
+export const AlbumsScreen = () => {
+  return (
+    <View>
+        <Text>hola <Icon name="calendar" size={20} color='red' /> </Text>
+    </View>
+  )
+}
